c9e52a3ae8af819719e5e8dc4393a7db77eafcf8
RafaelMaluf <104031822+RafaelMaluf@users.noreply.github.com>
2022-05-15 17:19:00 -0300

    Add files via upload

---
+++ tiaw sprint 2/dbprods.js
@@ -0,0 +1,23 @@
+var db = {
+    dados: [
+        {
+            titulo: 'placa de video 1650 tuf',
+            categoria: 'hardware',
+            foto: 'placadevideo.jfif',
+            descricao: 'placa de video com alto desempenho e preco que cabe dentro do bolso',
+            preco: 2000
+        }, {
+            titulo: 'Smartphone Samsung Galaxy S20 Fe',
+            categoria: 'Celulares',
+            foto: 'celular.jfif',
+            descricao: 'celular com desempenho medio e preco baixo',
+            preco: 2000
+        }, {
+            titulo: 'Monitor Gamer AOC Hero 23.8',
+            categoria: 'Monitores',
+            foto: 'monitor.jfif',
+            descricao: 'Monitor com melhor custo benefício feito para gaming',
+            preco: 1349.90
+        }
+    ]
+}
